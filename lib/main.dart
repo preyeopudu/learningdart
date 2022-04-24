fc6047032d259fb75c940d898cc34953f3ec0567
Opudu Preye <preyeopudu@gmail.com>
2022-04-24 19:50:14 +0100

    learnt gesture detector

--- lib/main.dart
+++ lib/main.dart
@@ -12,19 +12,15 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home:FirstPage()
-    );
+        debugShowCheckedModeBanner: false, home: FirstPage());
   }
 }
 
-
 class FirstPage extends StatefulWidget {
-  const FirstPage({ Key? key }) : super(key: key);
+  const FirstPage({Key? key}) : super(key: key);
 
   @override
   State<FirstPage> createState() => _FirstPageState();
@@ -33,68 +29,76 @@ class FirstPage extends StatefulWidget {
 class _FirstPageState extends State<FirstPage> {
   String buttonName = "Click me";
   int currentIndex = 0;
+  bool isClicked = false;
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-        appBar: AppBar(
-          elevation: 10,
-          title: const Text("App Title"),
-        ),
-        body: Center(
-            child: currentIndex == 0
-                ? Container(
-                    height: double.infinity,
-                    color: Colors.black,
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          ElevatedButton(
-                            onPressed: () {
-                              setState(() {
-                                buttonName = 'Clicked';
-                              });
-                            },
-                            child: Text(buttonName),
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 10,
+        title: const Text("App Title"),
+      ),
+      body: Center(
+          child: currentIndex == 0
+              ? Container(
+                  height: double.infinity,
+                  color: Colors.black,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              buttonName = 'Clicked';
+                            });
+                          },
+                          child: Text(buttonName),
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            onPrimary: Colors.white,
+                            primary: Colors.orange,
                           ),
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              onPrimary: Colors.white,
-                              primary: Colors.orange,
-                            ),
-                            onPressed: () {
-                              Navigator.of(context).push(MaterialPageRoute(
-                                builder: (BuildContext context) =>
-                                    const SecondPage(),
-                              ));
-                            },
-                            child: const Text("Next Page"),
-                          )
-                        ]),
-                  )
-                : currentIndex == 1
-                    ? Image.asset("images/profile.png")
-                    : currentIndex == 2
-                        ? Container(
-                            color: Colors.amber,
-                          )
-                        : Container()),
-        bottomNavigationBar: BottomNavigationBar(
-            elevation: 40,
-            items: const [
-              BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.shopping_cart), label: "Buy"),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.settings), label: "Settings")
-            ],
-            currentIndex: currentIndex,
-            onTap: (int index) {
-              setState(() {
-                currentIndex = index;
-              });
-            }),
-      );
+                          onPressed: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  const SecondPage(),
+                            ));
+                          },
+                          child: const Text("Next Page"),
+                        )
+                      ]),
+                )
+              : currentIndex == 1
+                  ? GestureDetector(
+                      child:isClicked? Image.asset("images/profile.png"):Image.network("https://w7.pngwing.com/pngs/38/708/png-transparent-car-mercedes-car-love-compact-car-vehicle.png"),
+                      onTap: () {
+                        setState(() {
+                          isClicked = !isClicked;
+                        });
+                      },
+                    )
+                  : currentIndex == 2
+                      ? Container(
+                          color: Colors.amber,
+                        )
+                      : Container()),
+      bottomNavigationBar: BottomNavigationBar(
+          elevation: 40,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_cart), label: "Buy"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: "Settings")
+          ],
+          currentIndex: currentIndex,
+          onTap: (int index) {
+            setState(() {
+              currentIndex = index;
+            });
+          }),
+    );
   }
 }
 
